9fc3d373762f49692326a636da9f8c8370cdcd40
DilshadDiwan <dilshaddiwan30@gmail.com>
2025-11-17 21:02:03 +0530

    func-2

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function sayhello(){
-    console.log("hello world ")
-}
-sayhello()
+// function sayhello(){
+//     console.log("hello world ")
+// }
+// sayhello()
+
+
+//Q -2
+
+// function add(a,b){
+//     console.log(`${a+b}`)
+// }
+// add(5,10)
+
+//Q-3
