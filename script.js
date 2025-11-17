2ab890324c196f26a789f0b79db9432bc691d985
DilshadDiwan <dilshaddiwan30@gmail.com>
2025-11-17 21:20:03 +0530

    question-3

--- script.js
+++ script.js
@@ -6,9 +6,14 @@
 
 //Q -2
 
-function add(a,b){
-    console.log(`${a+b}`)
-}
-add(5,10)
+// function add(a,b){
+//     console.log(`${a+b}`)
+// }
+// add(5,10)
 
 //Q-3
+
+function hi(name="guest"){
+    console.log(`hi ${name}`)
+}
+hi()
